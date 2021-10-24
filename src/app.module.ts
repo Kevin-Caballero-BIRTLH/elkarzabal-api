aba2bbf26e30b5ae08e6aef9a39a6de8a0efeb61
Kevin <kcaballero@birt.eus>
2021-10-24 21:49:58 +0200

    Environment configuration + ORM configuration
    Environment configuration + ORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './config/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './components/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get(config.database.host),
+        port: +configService.get<number>(config.database.port),
+        username: configService.get(config.database.username),
+        password: configService.get(config.database.password),
+        database: configService.get(config.database.name),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: process.env.NODE_ENV === 'development',
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
