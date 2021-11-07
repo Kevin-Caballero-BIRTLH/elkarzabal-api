7aaa8512137c5648b9f5f3e15cae4b4df411ad7a
k.caballero <92152425+kcaballeroulhi@users.noreply.github.com>
2021-11-07 16:21:11 +0100

    Swagger implemented (#16)
    Swagger implemented

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { config } from './config/config';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { UserModule } from './components/user/user.module';
+import { version, description } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +12,29 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.enableCors();
+
+  //#region Swagger config
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Elkarzabal')
+    .setDescription(description)
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .setVersion(version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig, {
+    include: [UserModule, AppModule],
+    ignoreGlobalPrefix: false,
+  });
+
+  SwaggerModule.setup('doc', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+  //#endregion
 
   await app.listen(configService.get(config.api.port), () => {
     console.log(
