5d150f4d681dde8a914f1de0271a3cbe1496d618
sbozal <84566430+sbozal@users.noreply.github.com>
2021-11-10 20:00:55 +0100

    get_all_products (#21) (#22)
    * get_all_products (#21)
    
    * product.module cambios para find all products
    
    * cambio endpoint

--- src/components/product/product.module.ts
+++ src/components/product/product.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductRepository } from './product.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductRepository])],
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService],
+  exports: [ProductService],
 })
 export class ProductModule {}
