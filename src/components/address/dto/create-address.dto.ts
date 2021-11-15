b9d52294b67d807d429df05cb365bf87e220f88c
Sara <sbozal@birt.eus>
2021-11-15 20:26:39 +0100

    create-address-dto

--- src/components/address/dto/create-address.dto.ts
+++ src/components/address/dto/create-address.dto.ts
@@ -1,1 +1,30 @@
-export class CreateAddressDto {}
+import { 
+    IsNotEmpty,
+    IsString,
+    IsNumber,
+    IsOptional, 
+    IsPostalCode,
+} from 'class-validator';
+
+export class CreateAddressDto {
+    @IsNotEmpty()
+    @IsString()
+    userId: number;
+
+    @IsNotEmpty()
+    @IsString()
+    state: string;
+
+    @IsNotEmpty()
+    @IsString()
+    city: string;
+
+    @IsNotEmpty()
+    @IsString()
+    street: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPostalCode()
+    postal_code: number;
+}
