fd5c9898850288413e4e46bbce3c3cc22da1f9c2
sbozal <84566430+sbozal@users.noreply.github.com>
2021-11-17 19:43:32 +0100

    Feature/address dto (#25)
    * create-address-dto
    
    * create address.repository
    
    * añadir length

--- src/components/address/dto/create-address.dto.ts
+++ src/components/address/dto/create-address.dto.ts
@@ -1,1 +1,33 @@
-export class CreateAddressDto {}
+import { 
+    IsNotEmpty,
+    IsString,
+    IsNumber,
+    Length, 
+    IsPostalCode,
+} from 'class-validator';
+
+export class CreateAddressDto {
+    @IsNotEmpty()
+    @IsString()
+    userId: number;
+
+    @IsNotEmpty()
+    @IsString()
+    @Length(50)
+    state: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Length(50)
+    city: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Length(100)
+    street: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPostalCode()
+    postal_code: number;
+}
