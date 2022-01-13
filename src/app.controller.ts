746f275e9f3fb388f7fe676f2b9217c7126233d7
Kevin <kcaballero@birt.eus>
2022-01-13 18:58:39 +0100

    Only admin can create user but limited to role vendor or customer
    Only admin can create user but limited to role vendor or customer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { AuthService } from './components/auth/auth.service';
 import { LocalAuthGuard } from './components/auth/local-auth.guard';
+import { ERole } from './components/role/entities/erole';
 import { CreateUserDto } from './components/user/dto/create-user.dto';
 import { UserService } from './components/user/user.service';
 import { Public } from './decorators/public.decorator';
+import { Roles } from './decorators/role.decorator';
 import { OpenApiBody } from './open-api/swagger/body';
 import { OpenApiResponse } from './open-api/swagger/response';
 import { OpenApiSummary } from './open-api/swagger/summary';
@@ -30,12 +39,16 @@ export class AppController {
     return this._authService.login(req.user);
   }
 
-  @Public()
   @Post('register')
   @ApiResponse(OpenApiResponse.APPregisterOk)
   @ApiResponse(OpenApiResponse.APPregisterBadRequest)
   @ApiOperation({ summary: OpenApiSummary.register })
+  @Roles(ERole.ADMIN)
   register(@Body() createUserDto: CreateUserDto) {
+    if (createUserDto.roleId === ERole.ADMIN)
+      throw new BadRequestException(
+        ' Possible roles are: (2 - VENDOR), (3 - CUSTOMER)',
+      );
     return this._userService.create(createUserDto);
   }
 }
